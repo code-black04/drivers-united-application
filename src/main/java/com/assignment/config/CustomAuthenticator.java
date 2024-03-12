eca6a109bb3491b71692abc40c6a2457b9bb4e6c
emiryucell <e.talhayucel@gmail.com>
2024-03-12 11:45:43 +0000

    Dummy data added to driver_details table. Driver controller, service and repository implemented. Driver entity changed. New methods added to driver mapper class. Basic Authenticator added. Error handlers and the other expansions will be added before the submission.

---
+++ src/main/java/com/assignment/config/CustomAuthenticator.java
@@ -0,0 +1,46 @@
+package com.assignment.config;
+
+import com.assignment.dtos.DriverDto;
+import com.assignment.entity.DriverEntity;
+import com.assignment.service.DriverUnitedAppLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CustomAuthenticator implements AuthenticationProvider {
+
+    @Autowired
+    private DriverUnitedAppLoginService service;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String userName = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        DriverEntity driver = service.login(userName, password);
+
+        if (driver == null) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+            return new UsernamePasswordAuthenticationToken(driver.getUserName(), driver.getPassword(), authorities);
+   }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/com/assignment/service/DriverUnitedAppLoginService.java
+++ src/main/java/com/assignment/service/DriverUnitedAppLoginService.java
@@ -1,7 +1,62 @@
 package com.assignment.service;
 
+import com.assignment.dtos.DriverDto;
+import com.assignment.entity.DriverEntity;
+import com.assignment.mapper.DriverDtoEntityMapper;
+import com.assignment.repository.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class DriverUnitedAppLoginService {
+    @Autowired
+    DriverRepository dao;
+
+    @Autowired
+    DriverDtoEntityMapper mapper;
+
+    public DriverDto registerDriver(DriverDto dto){
+       DriverEntity entity= mapper.convertToDriverEntity(dto);
+       entity= dao.save(entity);
+       return mapper.convertToDriverDto(entity);
+    }
+
+    public DriverEntity login(String userName,String password){
+        DriverEntity entity;
+        try {
+           entity = dao.findByUserNameAndPassword(userName,password).get();
+        }catch (NoSuchElementException exception){
+            return null;
+        }
+        return entity;
+    }
+    public DriverDto findByUserName(String userName){
+        DriverEntity entity=dao.findByUserName(userName).get();
+        return mapper.convertToDriverDto(entity);
+    }
+    public DriverDto findById(String id){
+        DriverEntity entity=dao.findById(id).get();
+        return mapper.convertToDriverDto(entity);
+    }
+    public List<DriverDto> findAllDrivers(){
+        List<DriverEntity> entityList=dao.findAll();
+        return mapper.convertToDriverDtoList(entityList);
+    }
+    public boolean removeDriverWithId(String id){
+        try {
+            dao.deleteById(id);
+            if(dao.existsById(id))
+                return false;
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+    public DriverDto changePassword(DriverDto driverDto){
+        DriverEntity entity=dao.save(mapper.convertToDriverEntity(driverDto));
+        return mapper.convertToDriverDto(entity);
+    }
 }
