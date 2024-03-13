fb341e9a53a3ea86a85b6092d8f1a9cea227be9f
code-black04 <purnimamaheshwari021@gmail.com>
2024-03-13 15:30:21 +0000

    updated CommonUtils

--- src/main/java/com/assignment/controller/FeedbackAndReportController.java
+++ src/main/java/com/assignment/controller/FeedbackAndReportController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Base64;
+import static com.assignment.CommonUtils.extractUsername;
 
 @RestController
 @RequestMapping(path = "/f")
@@ -26,20 +26,5 @@ public class FeedbackAndReportController {
                 String driverId = extractUsername(token);
                 FeedbackDto requestedFeedback = feedbackAndReportService.createFeedbackAndReport(feedback, driverId);
                 return new ResponseEntity<>(requestedFeedback, HttpStatus.CREATED);
-        }
-
-        public static String extractUsername(String base64EncodedString) {
-                // Decode the Base64 encoded string
-                byte[] decodedBytes = Base64.getDecoder().decode(base64EncodedString);
-                String decodedString = new String(decodedBytes);
-
-                // Assuming the format is "username:password", split by ":" and get the username
-                String[] parts = decodedString.split(":", 2);
-                return parts.length > 0 ? parts[0] : null; // Return the username part
-        }
-
-        public static void main(String[] args) {
-                String ec = "Basic dXNlcm5hbWU6YWJjMTIz".substring(6);
-                System.out.println(extractUsername(ec));
         }
 }
